91de1c43c352ff20ee952b016f382c3c6b701b19
takuminremon <73334013+takuminremon@users.noreply.github.com>
2020-12-08 16:27:39 +0900

    再度修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,21 @@ class User < ApplicationRecord
    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
    validates_format_of :password, with: PASSWORD_REGEX
 
+   with_options presence: true do
+   validates :nickname
+   validates :birthday
+   end
 
-   validates :nickname, presence: true 
    with_options presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]/}do
    validates :first_name   
    validates :last_name  
    end
+   
    with_options presence: true, format:{ with: /\A[ァ-ヶー－]+\z/}do
    validates :first_name_kana
    validates :last_name_kana
    end
-   validates :birthday, presence: true 
+    
    
    has_many :items
    has_many :purchases
